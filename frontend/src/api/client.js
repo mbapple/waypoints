d403bb30a28f2cce615c261ed392b2af4ad75416
mbapple <junkmbfl10000@gmail.com>
2026-01-01 23:49:21 -0600

    Fixed hardcoding of IP

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Prefer environment variable, fall back to local dev
 //export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001/api";
-export const API_BASE_URL = "http://192.168.31.101:3001/api"
+export const API_BASE_URL = "http://localhost:3001/api"
 export const API_ORIGIN = API_BASE_URL.replace(/\/?api\/?$/, "");
 
 export const api = axios.create({ baseURL: API_BASE_URL });
